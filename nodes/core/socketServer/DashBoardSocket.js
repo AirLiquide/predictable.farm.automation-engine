8001ac4ce71a553d2bfe15e8c4ed916ddf4db5c9
Brice Culas <culas.brice@gmail.com>
2017-03-22 12:29:52 +0100

    added connection to cloud dashboard

--- nodes/core/socketServer/DashBoardSocket.js
+++ nodes/core/socketServer/DashBoardSocket.js
@@ -3,39 +3,62 @@
  */
 "use strict"
 
-var cloudAddress = '';
+const crypto = require('crypto');
+var socketClient = require('socket.io-client');
+var cloudAddress = 'http://192.168.222.197:9000';
 
-class DashBoardSocket{
+var farmID = process.env.FARM_ID || "";
+var secretKey = process.env.PREDICTABLE_KEY || "";
 
-    constructor(type){
-        this.socket = require('socket.io-client')('http://127.0.0.1:80/');
-        this.cloudSocket = require('socket.io-client')(cloudAddress);
-        this.socket.on('connect', function(){
-            console.log(type+" node connected to dashboard");
+
+function encrypt(text) {
+    var crypted = crypto.createHmac('sha256', secretKey).update(text).digest('hex');
+    return crypted;
+}
+
+class DashBoardSocket {
+
+    constructor(type) {
+        this.socket = socketClient('http://127.0.0.1:80/');
+        this.cloudSocket = socketClient(cloudAddress,{
+            query: "farmId=" + farmID
+        });
+        this.socket.on('connect', function () {
+            console.log(type + " node connected to dashboard");
         });
 
-        this.socket.on('connect_error', function(error){
+        this.socket.on('connect_error', function (error) {
             //console.log(error);
         });
         this.socket.emit("hello");
 
-        this.cloudSocket.on('connect', function(){
-            console.log(type+" node connected to cloud dashboard");
+        var cs = this.cloudSocket;
+        this.cloudSocket.on('connect', function () {
+
+            cs.on('connect_error', function (error) {
+                //console.log(error);
+            });
+            cs.on("authenticate", function(msg){
+                var hash = encrypt(msg);
+                cs.emit('authenticate', hash);
+
+            });
+            cs.on("authenticated", function(msg){
+                console.log(type + " node connected to cloud dashboard");
+            });
         });
 
-        this.cloudSocket.on('connect_error', function(error){
-            //console.log(error);
-        });
-        this.cloudSocket.emit("hello");
     }
 
-    emit(event, data){
-        this.socket.emit(event,data);
+    emit(event, data) {
+        this.socket.emit(event, data);
+        this.cloudSocket.emit(event, data);
     }
 
-    close(){
+    close() {
 
     }
+
 
 
 }
