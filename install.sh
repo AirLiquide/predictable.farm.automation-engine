8e512c5f43a1b785a7ffb1545a7e0212bc072156
Batiga Faouzane <fb@enyosolutions.com>
2017-07-12 13:05:01 +0200

    feat: install du theme

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ">> Copying theme settings to node-red"
+cp ./theme/settings.js ~/.node-red/
+node_red_dir=`which node-red`
+node_red_dir=`dirname $node_red_dir`
+node_red_dir=$node_red_dir"/../lib/node_modules/node-red"
+
+echo ">> found node-red dir: " $node_red_dir
+mkdir -p "$node_red_dir/public/vendor"
+echo ">> Copying theme assets to node-red"
+cp -r ./theme/predictable-ui $node_red_dir/public/vendor
