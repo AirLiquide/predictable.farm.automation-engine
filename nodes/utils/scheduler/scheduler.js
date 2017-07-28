2375dddfe013ecdc2d8c9c5befd13d03f3d188d4
Brice Culas <brice.culas@airliquide.com>
2017-07-28 14:56:01 +0200

    remade the scheduler node

--- nodes/utils/scheduler/scheduler.js
+++ nodes/utils/scheduler/scheduler.js
@@ -17,62 +17,40 @@
 module.exports = function(RED) {
     "use strict";
     var cron = require("cron");
+    var schedule = require('node-schedule');
 
     function SchedulerNode(n) {
         RED.nodes.createNode(this,n);
         this.topic = n.topic;
         this.repeat = n.repeat;
-        this.crontab = n.crontab;
+        console.log(n.crontab);
+        this.crontab = JSON.parse(n.crontab);
         this.once = n.once;
         var node = this;
         this.interval_id = null;
         this.cronjob = null;
+        this.timeDays = n.timeDays;
+        this.jobs = [];
 
-        if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-            this.repeat = this.repeat * 1000;
-            if (RED.settings.verbose) { this.log(RED._("repeat = __repeat__",this)); }
-            this.interval_id = setInterval( function() {
-                node.send({});
-            }, this.repeat );
-        } else if (this.crontab) {
-            if (RED.settings.verbose) { this.log(RED._("crontab = __crontab__",this)); }
-            this.cronjob = new cron.CronJob(this.crontab,
-                function() {
-                    node.send({});
-                },
-                null,true);
+        if (this.crontab.valid){
+            this.crontab.value.forEach(function (el) {
+                var job = schedule.scheduleJob(el, function(){
+                    node.send({
+                        payload:"hello"
+                    })
+                });
+
+                node.jobs.push(job);
+            })
+
+            this.on("close", function () {
+                this.jobs.forEach(function (j) {
+                    j.cancel();
+                })
+            });
         }
 
-        if (this.once) {
-            setTimeout( function() { node.send({payload:"test"}); }, 100 );
-        }
     }
 
     RED.nodes.registerType("scheduler",SchedulerNode);
-
-    SchedulerNode.prototype.close = function() {
-        if (this.interval_id != null) {
-            clearInterval(this.interval_id);
-            if (RED.settings.verbose) { this.log(RED._("stopped")); }
-        } else if (this.cronjob != null) {
-            this.cronjob.stop();
-            if (RED.settings.verbose) { this.log(RED._("stopped")); }
-            delete this.cronjob;
-        }
-    }
-
-    RED.httpAdmin.post("/inject/:id", RED.auth.needsPermission("inject.write"), function(req,res) {
-        var node = RED.nodes.getNode(req.params.id);
-        if (node != null) {
-            try {
-                node.receive();
-                res.sendStatus(200);
-            } catch(err) {
-                res.sendStatus(500);
-                node.error(RED._("Inject failed: __error__",{error:err.toString()}));
-            }
-        } else {
-            res.sendStatus(404);
-        }
-    });
-}
+};
