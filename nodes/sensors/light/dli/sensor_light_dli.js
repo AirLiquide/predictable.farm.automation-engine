665136155a3e20867ac64d0aebf8213c0bac4e99
Brice Culas <brice.culas@airliquide.com>
2017-08-01 15:26:34 +0200

    removed console log

--- nodes/sensors/light/dli/sensor_light_dli.js
+++ nodes/sensors/light/dli/sensor_light_dli.js
@@ -49,7 +49,6 @@ module.exports = function (RED) {
         this.updateValue = function(){
             var v = DLIStore.getDLI(node.deviceId);
             node.dli = v;
-            console.log(v)
             var msg = {
                 sensor_value : node.dli.toString(),
                 sensor_type : 'light_dli',
