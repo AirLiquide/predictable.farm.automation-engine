4944048d7fd82d5b3af16b43ad14e4b821c9cd10
Brice Culas <brice.culas@airliquide.com>
2017-08-03 12:26:14 +0200

    improved email node

--- nodes/utils/email/predictable_email.js
+++ nodes/utils/email/predictable_email.js
@@ -26,6 +26,9 @@ module.exports = function(RED) {
         this.outserver = "mail.gandi.net";
         this.outport = "465";
         this.secure = true;
+        this.mail = n.mail;
+        this.content = n.content;
+        this.subject = n.subject;
         var flag = false;
         if (this.credentials && this.credentials.hasOwnProperty("userid")) {
             this.userid = this.credentials.userid;
@@ -69,40 +72,18 @@ module.exports = function(RED) {
             if (msg.hasOwnProperty("payload")) {
                 if (smtpTransport) {
                     node.status({fill:"blue",shape:"dot",text:"email.status.sending"});
-                    if (msg.to && node.name && (msg.to !== node.name)) {
-                        node.warn(RED._("node-red:common.errors.nooverride"));
-                    }
-                    var sendopts = { from: ((msg.from) ? msg.from : node.userid) };   // sender address
-                    sendopts.to = node.name || msg.to; // comma separated list of addressees
-                    if (node.name === "") {
+                    var sendopts = { from: node.userid };   // sender address
+                    sendopts.to = node.mail; // comma separated list of addressees
+                    /*if (node.mail === "") {
                         sendopts.cc = msg.cc;
                         sendopts.bcc = msg.bcc;
-                    }
-                    sendopts.subject = msg.topic || msg.title || "Message from Node-RED"; // subject line
-                    if (msg.hasOwnProperty("envelope")) { sendopts.envelope = msg.envelope; }
-                    if (Buffer.isBuffer(msg.payload)) { // if it's a buffer in the payload then auto create an attachment instead
-                        if (!msg.filename) {
-                            var fe = "bin";
-                            if ((msg.payload[0] === 0xFF)&&(msg.payload[1] === 0xD8)) { fe = "jpg"; }
-                            if ((msg.payload[0] === 0x47)&&(msg.payload[1] === 0x49)) { fe = "gif"; } //46
-                            if ((msg.payload[0] === 0x42)&&(msg.payload[1] === 0x4D)) { fe = "bmp"; }
-                            if ((msg.payload[0] === 0x89)&&(msg.payload[1] === 0x50)) { fe = "png"; } //4E
-                            msg.filename = "attachment."+fe;
-                        }
-                        var fname = msg.filename.replace(/^.*[\\\/]/, '') || "file.bin";
-                        sendopts.attachments = [ { content:msg.payload, filename:fname } ];
-                        if (msg.hasOwnProperty("headers") && msg.headers.hasOwnProperty("content-type")) {
-                            sendopts.attachments[0].contentType = msg.headers["content-type"];
-                        }
-                        // Create some body text..
-                        sendopts.text = RED._("email.default-message",{filename:fname, description:(msg.description||"")});
-                    }
-                    else {
-                        var payload = RED.util.ensureString(msg.payload);
+                    }*/
+                    sendopts.subject = node.subject || "Message from Node-RED"; // subject line
+                        var payload = RED.util.ensureString(node.content);
                         sendopts.text = payload; // plaintext body
                         if (/<[a-z][\s\S]*>/i.test(payload)) { sendopts.html = payload; } // html body
-                        if (msg.attachments) { sendopts.attachments = msg.attachments; } // add attachments
-                    }
+
+                    console.log(sendopts);
                     smtpTransport.sendMail(sendopts, function(error, info) {
                         if (error) {
                             node.error(error,msg);
