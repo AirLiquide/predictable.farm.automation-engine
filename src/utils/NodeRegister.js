42ffc440dc6d39bea4241b913aa11508aed70277
Cyril ferte <cyril.ferte@airliquide.com>
2017-12-01 11:21:08 +0100

    fix on off bug2

--- src/utils/NodeRegister.js
+++ src/utils/NodeRegister.js
@@ -106,7 +106,7 @@ class NodeRegister {
             this.node.status({
                 fill: "green",
                 shape: "dot",
-                text: data.device_id + " / Value : " + ((data.sensor_value == 0) ? "ON" : "OFF")
+                text: data.device_id + " / Value : " + ((data.sensor_value == 0) ? "OFF" : "ON")
             });
         }
         else if (this.node.nodeType == "sensor_light_dli") {
